c9ad9876521a0cbe8dffe89750033d5069ffa8ce
yxwbruceboy <1970368332@qq.com>
2016-08-10 14:19:43 +0800

    什么鬼

---
+++ Baby/src/com/example/baby/TalkingAboutActivity.java
@@ -0,0 +1,96 @@
+package com.example.baby;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class TalkingAboutActivity extends Activity{
+	ImageView imageview_touxiang;
+	ListView listview;
+	TextView textview_nicheng;
+	TextView textview_pinglun;
+	TextView textview_date;
+	ArrayAdapter arrayAdapter;
+	SimpleAdapter simpleAdapter;
+	List<String> lists=new ArrayList<String>();
+	List<Map<String,Object>> listData=new ArrayList<Map<String,Object>>();
+	protected void onCreate(Bundle bundle){
+		super.onCreate(bundle);
+		setContentView(R.layout.talkingabout_layout);
+		listview=(ListView)findViewById(R.id.taolun_listview);
+		imageview_touxiang=(ImageView)findViewById(R.id.touxiang_imageview);
+		textview_nicheng=(TextView)findViewById(R.id.name_textview);
+		textview_pinglun=(TextView)findViewById(R.id.talk_textview);
+		textview_date=(TextView)findViewById(R.id.time_textview);
+		arrayAdapter=new ArrayAdapter(TalkingAboutActivity.this,R.layout.talkingabout_listview_layout,R.id.name_textview);
+		String[] keys={"imageview_touxiang","textview_nicheng","textview_pinglun","textview_date"};
+		int[] viewIds={R.id.touxiang_imageview,R.id.name_textview,R.id.talk_textview,R.id.time_textview};
+		simpleAdapter=new SimpleAdapter(TalkingAboutActivity.this,listData,R.layout.talkingabout_listview_layout,keys,viewIds);
+		listview.setAdapter(simpleAdapter);
+		getData();
+	}
+	OnClickListener clickListener=new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent intent=new Intent(TalkingAboutActivity.this,GuanFangActivity.class);
+			startActivity(intent);
+		}};
+		public void getData(){
+			Map<String,Object> map=new HashMap<String,Object>();
+			map.put("imageview_touxiang", R.drawable.touxiang_one);
+			map.put("textview_nicheng", "paxoap");
+			map.put("textview_pinglun", "说说你们俩口子的口味是否相同");
+			map.put("textview_data", "2016/05/06");
+			listData.add(map);
+			map=new HashMap<String,Object>();
+			map.put("imageview_touxiang", R.drawable.touxiang_two);
+			map.put("textview_nicheng", "paxoap");
+			map.put("textview_pinglun", "雨雨欣宝宝的穿衣搭配");
+			map.put("textview_data", "2016/05/06");
+			listData.add(map);
+			map=new HashMap<String,Object>();
+			map.put("imageview_touxiang", R.drawable.touxiang_three);
+			map.put("textview_nicheng", "paxoap");
+			map.put("textview_pinglun", "看见你老公手机里有簧片，你会生气么？");
+			map.put("textview_data", "2016/05/06");
+			listData.add(map);
+			map=new HashMap<String,Object>();
+			map.put("imageview_touxiang", R.drawable.touxiang_four);
+			map.put("textview_nicheng", "paxoap");
+			map.put("textview_pinglun", "别忽视“话少又乖巧”的孩子");
+			map.put("textview_data", "2016/05/06");
+			listData.add(map);
+			map=new HashMap<String,Object>();
+			map.put("imageview_touxiang", R.drawable.touxiang_five);
+			map.put("textview_nicheng", "paxoap");
+			map.put("textview_pinglun", "宝宝老吐奶");
+			map.put("textview_data", "2016/05/06");
+			listData.add(map);
+			map=new HashMap<String,Object>();
+			map.put("imageview_touxiang",R.drawable.touxiang_six);
+			map.put("textview_nicheng", "paxoap");
+			map.put("textview_pinglun", "求推荐一款不易上火的奶粉3段的");
+			map.put("textview_data", "2016/05/06");
+			listData.add(map);
+			map=new HashMap<String,Object>();
+			map.put("imageview_touxiang", R.drawable.touxiang_seven);
+			map.put("textview_nicheng", "paxoap");
+			map.put("textview_pinglun", "急急急急急急急，宝宝拉的屎有颗粒是不是消化不良？需要怎么办？");
+			map.put("textview_data", "2016/05/06");
+			listData.add(map);
+		}
+}
