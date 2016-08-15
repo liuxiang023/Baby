f7405c716816d9a9a681dac053896f0f4c64c50d
liuxiang023 <549390476@qq.com>
2016-08-15 14:24:13 +0800

    导航栏，和listview的细节修改

--- Baby/src/com/example/baby/LiaoTianActivity.java
+++ Baby/src/com/example/baby/LiaoTianActivity.java
@@ -1,13 +1,11 @@
 package com.example.baby;
 
-import com.method.Homepage_Fragment;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.ImageButton;
 
 public class LiaoTianActivity extends Activity{
